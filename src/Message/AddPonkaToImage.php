2b13d845ee78e18d3b1a29a9390406e5498cc366
Jahir Hossain <j.hossain@reply.de>
2023-03-10 17:19:40 +0100

    Move logic to the message handler class

---
+++ src/Message/AddPonkaToImage.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Message;
+
+use App\Entity\ImagePost;
+
+class AddPonkaToImage
+{
+
+    /**
+     * @var ImagePost
+     */
+    private $imagePost;
+
+    public function __construct(ImagePost $imagePost)
+    {
+        $this->imagePost = $imagePost;
+    }
+
+    /**
+     * @return ImagePost
+     */
+    public function getImagePost(): ImagePost
+    {
+        return $this->imagePost;
+    }
+
+}
