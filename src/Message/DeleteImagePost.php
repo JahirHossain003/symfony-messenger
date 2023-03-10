597e01bb6deaaabae2f0d8c8cb8ebaecc44d93f9
Jahir Hossain <j.hossain@reply.de>
2023-03-10 22:27:18 +0100

    refactor deletion mechanism

---
+++ src/Message/DeleteImagePost.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Message;
+
+use App\Entity\ImagePost;
+
+class DeleteImagePost
+{
+
+    /**
+     * @var ImagePost
+     */
+    private $imagePost;
+
+    public function __construct(ImagePost $imagePost)
+    {
+        $this->imagePost = $imagePost;
+    }
+
+    /**
+     * @return ImagePost
+     */
+    public function getImagePost(): ImagePost
+    {
+        return $this->imagePost;
+    }
+
+}
