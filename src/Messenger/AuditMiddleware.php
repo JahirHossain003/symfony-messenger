728ef80b274723482add943454c3328ec69b62c2
Jahir Hossain <j.hossain@reply.de>
2023-03-13 14:04:13 +0100

    add separate logger channel

--- src/Messenger/AuditMiddleware.php
+++ src/Messenger/AuditMiddleware.php
@@ -2,20 +2,50 @@
 
 namespace App\Messenger;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\Middleware\MiddlewareInterface;
 use Symfony\Component\Messenger\Middleware\StackInterface;
+use Symfony\Component\Messenger\Stamp\HandledStamp;
+use Symfony\Component\Messenger\Stamp\ReceivedStamp;
 
 class AuditMiddleware implements MiddlewareInterface
 {
+    /**
+     * @var LoggerInterface
+     */
+    private $messengerAudit;
+
+    public function __construct(LoggerInterface $messengerAudit)
+    {
+
+        $this->messengerAudit = $messengerAudit;
+    }
+
     public function handle(Envelope $envelope, StackInterface $stack): Envelope
     {
         if (null == $envelope->last(UniqueIdStamp::class)) {
             $envelope = $envelope->with(new UniqueIdStamp());
         }
 
-        dump($envelope->last(UniqueIdStamp::class));
+        /** @var UniqueIdStamp $stamp */
+        $stamp = $envelope->last(UniqueIdStamp::class);
 
-        return $stack->next()->handle($envelope, $stack);
+        $envelope = $stack->next()->handle($envelope, $stack);
+
+        $context = [
+            'id' => $stamp->getUuid(),
+            'class' => get_class($envelope->getMessage())
+        ];
+
+        if ($envelope->last(ReceivedStamp::class)) {
+            $this->messengerAudit->info('[{id}] Received {class}', $context);
+        } elseif ($envelope->last(HandledStamp::class)) {
+            $this->messengerAudit->info('[{id}] Handled {class}', $context);
+        } else {
+            $this->messengerAudit->info('[{id}] Sent {class}', $context);
+        }
+
+        return $envelope;
     }
 }
