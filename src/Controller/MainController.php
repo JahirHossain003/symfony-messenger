3aa5aabea95d36f9ed611f7303927c2981fe2722
Jahir Hossain <j.hossain@reply.de>
2023-03-18 07:53:28 +0100

    Use Query bus to take a image count

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Message\Query\GetTotalImageCount;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Stamp\HandledStamp;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
@@ -11,8 +14,16 @@ class MainController extends AbstractController
     /**
      * @Route("/")
      */
-    public function homepage(): Response
+    public function homepage(MessageBusInterface $queryBus): Response
     {
-        return $this->render('main/homepage.html.twig');
+        $envelop = $queryBus->dispatch(new GetTotalImageCount());
+
+        /** @var HandledStamp $handled */
+        $handled = $envelop->last(HandledStamp::class);
+        $imageCount = $handled->getResult();
+
+        return $this->render('main/homepage.html.twig', [
+            'totalImages' => $imageCount
+        ]);
     }
 }
